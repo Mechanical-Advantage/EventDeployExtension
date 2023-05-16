49cdf7876e31b21221c0a7d474e5017ca4908fff
Jonah <47046556+jonahb55@users.noreply.github.com>
2023-05-15 22:07:34 -0400

    Fix formatting

--- extension.js
+++ extension.js
@@ -1,41 +1,50 @@
-const vscode = require("vscode")
-const cp = require("child_process")
+const vscode = require("vscode");
+const cp = require("child_process");
 
-const branchPrefix = "event"
+const branchPrefix = "event";
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  function runCommand(command, callback) {
+    cp.exec(
+      command,
+      { cwd: vscode.workspace.workspaceFolders[0].uri.fsPath },
+      (err, stdout, stderr) => {
+        callback(stdout);
+      }
+    );
+  }
 
-	function runCommand(command, callback) {
-		cp.exec(command, { cwd: vscode.workspace.workspaceFolders[0].uri.fsPath }, (err, stdout, stderr) => {
-			callback(stdout)
-		})
-	}
+  context.subscriptions.push(
+    vscode.commands.registerCommand("event-deploy-wpilib.deploy", function () {
+      runCommand("git branch --show-current", (branch) => {
+        if (!branch.startsWith(branchPrefix)) {
+          vscode.window.showErrorMessage(
+            'Current branch "' +
+              branch.trim() +
+              '" is not a valid event branch.'
+          );
+        } else {
+          var commitMessage = 'Update at "' + new Date().toLocaleString() + '"';
 
-	context.subscriptions.push(vscode.commands.registerCommand("event-deploy.deploy", function () {
-		runCommand("git branch --show-current", branch => {
-			if (!branch.startsWith(branchPrefix)) {
-				vscode.window.showErrorMessage("Current branch \"" + branch.trim() + "\" is not a valid event branch.")
-			} else {
-				var commitMessage = "Update at \"" + new Date().toLocaleString() + "\""
-
-				runCommand("git add -A", () => {
-					runCommand("git commit -m '" + commitMessage + "'", () => {
-						vscode.commands.executeCommand("wpilibcore.deployCode")
-					})
-				})
-			}
-		})
-	}))
+          runCommand("git add -A", () => {
+            runCommand("git commit -m '" + commitMessage + "'", () => {
+              vscode.commands.executeCommand("wpilibcore.deployCode");
+            });
+          });
+        }
+      });
+    })
+  );
 }
 // @ts-ignore
-exports.activate = activate
+exports.activate = activate;
 
-function deactivate() { }
+function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
